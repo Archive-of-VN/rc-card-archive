277d9cb83128ade291ac8ae6da009b4900bf4146
kosjenka <asteromataa@gmail.com>
2026-07-18 22:22:08 -0700

    something was fucked up

--- js/main.js
+++ js/main.js
@@ -77,17 +77,6 @@ function formatRewardShort(card) {
   `;
 }
 
-// giving cups their own class to move them upwards
-
-const iconClass = isCups ? "reward-icon cup-icon" : "reward-icon";
-
-return `
-<span class="reward">
-  ${card.rewardAmount}
-  <img src="${icon}" alt="${card.reward}" class="${iconClass}">
-</span>
-`;
-
 // --- 3. Initialisation & data loading ---
 
 async function loadCards() {
